f7b94c649b4aefa1ff7d948203df91f89a6a8e6c
dabuzida <dabuzida@gmail.com>
2021-12-30 17:25:02 +0900

    요일맞추기 완성

--- match_the_day.js
+++ match_the_day.js
@@ -1,7 +1,7 @@
 const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 const a = 5; // "TUE"
 const b = 24;
-const numOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 월 별 일 수
+const numOfDays = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 월 별 일 수
 console.log(numOfDays.length);
 /* 
 1.1은 금요일
@@ -10,4 +10,16 @@ console.log(numOfDays.length);
 30일까지있는 달: 4 6 9 11
 31일까지있는 달: 1 3 5 7 8 10 12
  */
+
+let sumOfNum = 0;
+let x;
+for(let i=0; i<a; i++){
+    sumOfNum += numOfDays[i];
+}
+x = (sumOfNum + b)%7;
+
+console.log(day[(4+x)%7]);
+
+
+
 
