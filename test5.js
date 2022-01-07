84535f959ba512d2f06468d754c878b8c4392de2
dabuzida <dabuzida@gmail.com>
2022-01-07 17:38:11 +0900

    자바튜토리얼 코드 실행연습

--- test5.js
+++ test5.js
@@ -1,27 +1,68 @@
-/* let user = {name: "John"};
-let admin = {name: "Admin"};
+const { reduce } = require('lodash');
+
+let user = {
+	name: 'John',
+	// ff: () => console.log(this.name)
+};
+let admin = { name: 'Admin' };
 
 function sayHi() {
-    console.log(this.name);
+	console.log(this.name);
 }
 
 user.f = sayHi;
 admin.f = sayHi;
-
 console.log(user.f);
-user.f(); */
+user.f();
 
-let user = {
+user.ff = () => console.log(this.name);
+console.log(user.ff);
+user.ff();
+
+let userr = {
 	firstName: '보라',
+	sayHellow() {
+		console.log(`1. ${this.firstName}`);
+	},
 	sayHi() {
-		let arrow = () => console.log(this.firstName);
+		let arrow = () => console.log(`2. ${this.firstName}`);
 		arrow();
+	},
+	xx() {
+		function arrow2() {
+			console.log('22. ' + this.firstName);
+		}
+		arrow2();
+	},
+	sayHello() {
+		let xx = function () {
+			console.log(`3. ${this.firstName}`);
+		};
+		xx();
+	},
+	fin: () => console.log(`4. ${this.firstName}`),
+};
+
+userr.sayHellow();
+userr.sayHi(); // 보라
+userr.xx(); // 보라
+userr.sayHello(); // undefined
+userr.fin(); // undefined
+console.log(userr.fin);
+
+let ladder = {
+	step: 0,
+	up() {
+		this.step++;
+		return this;
+	},
+	down() {
+		this.step--;
+	},
+	showStep: function () {
+		// 사다리에서 몇 번째 단에 올라와 있는지 보여줌
+		alert(this.step);
 	},
 };
 
-
-
-user.sayHi(); // 보라
-
-var xx = 11;
-
+ladder.up().up().down().showStep();
