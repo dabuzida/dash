8c290df55bb3807b9ba5fe6b28f59ad2864347be
dabuzida <dabuzida@gmail.com>
2022-04-20 18:03:19 +0900

    테스팅~

--- dart_test3.dart
+++ dart_test3.dart
@@ -3,7 +3,7 @@
 import 'dart:ffi';
 
 void main() {
-  Person p = Person();
+  /* Person p = Person();
   print(p.nameChange(p.name));
 
   People pp = People();
@@ -12,7 +12,35 @@ void main() {
   print(a[a.length - 1]);
 
   var acceptor = new List.filled(5, null, growable: false);
-  print(acceptor);
+  print(acceptor); */
+
+  String x = '6.';
+  double y = double.parse(x);
+  double z = double.parse('0.6');
+  print(y.runtimeType);
+  print(z);
+
+  if (y is double) {
+    var num1212 = 12;
+  }
+  print(num1212);
+
+  /* var x = 0 / 0; //NaN
+  var y = 5 / 0; //Infinity
+  var z = 11; // Finite
+  var i = -451; // Negative
+  print(x);
+  if (y.isInfinite) {
+    print(y);
+  }
+  if (z.isFinite) {
+    print(z);
+  }
+  if (i.isNegative) {
+    print(i);
+  }
+  print( 33+y);
+  print(y + 33); */
 }
 
 class Person {
