d8a05d26332ea723e039dee5d47259a2fac46ef7
dabuzida <dabuzida@gmail.com>
2022-06-27 18:08:37 +0900

    1

--- 비동기 프로그래밍 js/생활코딩/프라미스2.js
+++ 비동기 프로그래밍 js/생활코딩/프라미스2.js
@@ -1,6 +1,25 @@
-let ss1 = new Promise((resolve, reject) => resolve('resolved ok!'));
+function delay(ms) {
+	return new Promise((resolve, reject) => setTimeout(resolve, ms));
+}
+
+console.log(1);
+let ss1 = new Promise((resolve, reject) => {
+	setTimeout(() => resolve('resolved ok!'), 2000);
+});
 console.log(ss1);
 ss1.then(resolve => console.log(resolve));
 ss1.then(resolve => console.log(typeof resolve));
+console.log(2);
+
+// console.log(1);
+// setTimeout(() => {
+// 	console.log(123);
+// }, 2000);
+// console.log(2);
+
 //  .then(data => console.log(data));
 // new Promise((resolve, reject) => resolve('resolved ok!')).then(data => console.log(data));
+
+// new Promise((resolve, reject) => {
+// 	setTimeout(() => resolve('결과'), 2000);
+// })
